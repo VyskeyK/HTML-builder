dddbb658e2d2d06d3057f1918bc89de394620d58
VyskeyK <robintyan96@gmail.com>
2022-11-03 15:22:25 +0300

    feat: add goodbye message

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -6,5 +6,14 @@ const output = fs.createWriteStream('text.txt');
 stdout.write('Привет!\nНиже введи свое сообщение, и оно будет записано в файл text.txt\nЧтобы закончить, введи exit или Ctrl+C\n');
 
 stdin.on('data', function(chunk) {
+  if (chunk.toString().trim() === 'exit') {
+    stdout.write('Ввод завершен\nПока!')
+    process.exit();
+  }
   output.write(chunk);
 })
+
+process.on('SIGINT', function() {
+  stdout.write('Ввод завершен\nПока!')
+  process.exit();
+});

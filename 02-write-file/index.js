6570b5e57f9787bbd20789b855cb81f8bdef2de9
VyskeyK <robintyan96@gmail.com>
2022-11-03 14:33:19 +0300

    feat: write input to text.txt

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,3 +1,10 @@
 const { stdin, stdout } = process;
+const fs = require('fs');
 
-stdout.write('Привет!\nНиже введи свое сообщение, и оно будет записано в файл text.txt\nЧтобы закончить, введи exit или Ctrl+C');
+const output = fs.createWriteStream('text.txt');
+
+stdout.write('Привет!\nНиже введи свое сообщение, и оно будет записано в файл text.txt\nЧтобы закончить, введи exit или Ctrl+C\n');
+
+stdin.on('data', function(chunk) {
+  output.write(chunk);
+})

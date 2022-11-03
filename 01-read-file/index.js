3989ba8deded502a240d2ad2bb217a9eb2fccf5f
VyskeyK <robintyan96@gmail.com>
2022-11-03 13:51:09 +0300

    feat: output text.txt content to console

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,3 +1,9 @@
 const path = require('path');
 const pathToText = path.join(__dirname, 'text.txt');
-console.log(pathToText);
+
+const fs = require('fs');
+const stream = fs.createReadStream(pathToText, {encoding: 'UTF-8'});
+
+stream.on('data', function(chunk) {
+  console.log(chunk);
+});

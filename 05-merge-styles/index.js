da940b5b854c51c2e96a86bd57d40206b4eaa568
VyskeyK <robintyan96@gmail.com>
2022-11-06 15:26:47 +0300

    feat: create bundle.css

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,9 +1,10 @@
 const { stdout } = require('process');
 const path = require('path');
-const { readdir } = require('fs/promises');
+const { readdir, readFile } = require('fs/promises');
+const { createWriteStream } = require('fs');
 
 const stylesDir = path.join(__dirname, 'styles');
-const bundleDir = path.join(__dirname, 'project-dist');
+const bundlePath = path.join(__dirname, 'project-dist', 'bundle.css');
 
 bundleStyles();
 
@@ -11,10 +12,12 @@ async function bundleStyles() {
   try {
     const files = await readdir(stylesDir, {withFileTypes: true});
     const filteredFiles = files.filter( (file) => file.isFile() && path.extname(file.name) === '.css');
+    const stream = createWriteStream(bundlePath, 'utf-8');
     for (const file of filteredFiles) {
-      console.log(file.name)
+      const fileText = await readFile(path.join(stylesDir, file.name), 'utf-8');
+      stream.write(`${fileText}\n`);
     }
-
+    stdout.write('The bundle was created successfully');
   } catch (err) {
     console.error(err.message);
   }

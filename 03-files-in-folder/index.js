bd8e3bd33ab0ec510088d6a6ed65e9c29b6699bc
VyskeyK <robintyan96@gmail.com>
2022-11-06 14:20:35 +0300

    feat: add format bytes function

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -13,10 +13,12 @@ async function findFilesInDir(pathToFolder) {
       const filePath = path.join(pathToFolder, file.name);
       const fileStat = await stat(filePath);
       const fileInfo = (file.name).split('.');
-      fileInfo.push(fileStat.size);
-      stdout.write('\n' + fileInfo.join(' - '));
+      fileInfo.push(formatBytes(fileStat.size));
+      stdout.write(fileInfo.join(' - ') + '\n');
     }
   } catch (err) {
     console.error(err);
   }
 }
+
+function formatBytes(a,b=2){if(!+a)return"0 Bytes";const c=0>b?0:b,d=Math.floor(Math.log(a)/Math.log(1024));return`${parseFloat((a/Math.pow(1024,d)).toFixed(c))} ${["Bytes","KB","MB","GB","TB","PB","EB","ZB","YB"][d]}`}

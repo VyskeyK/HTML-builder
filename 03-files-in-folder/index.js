ae1a20b216741e83ff6fcd7685ee6f918a4b3b5d
VyskeyK <robintyan96@gmail.com>
2022-11-06 14:11:44 +0300

    feat: find files size

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,5 +1,5 @@
 const { stdout } = process;
-const {readdir} = require('fs/promises');
+const {readdir, stat} = require('fs/promises');
 const path = require('path');
 const pathToFolder = path.join(__dirname, 'secret-folder');
 
@@ -7,10 +7,14 @@ stdout.write(pathToFolder);
 
 findFilesInDir(pathToFolder);
 
-async function findFilesInDir(path) {
+async function findFilesInDir(pathToFolder) {
   try {
-    const files = await readdir(path);
-    for (const file of files) stdout.write(file);
+    const files = await readdir(pathToFolder, {withFileTypes: true});
+    for (const file of files) {
+      const filePath = path.join(pathToFolder, file.name);
+      const fileStat = await stat(filePath);
+      stdout.write('\n' + file.name + ' ' + fileStat.size);
+    }
   } catch (err) {
     console.error(err);
   }

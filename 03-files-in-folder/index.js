6b09fb6d23a3439046baea145b8a2cd5de4a0dfb
VyskeyK <robintyan96@gmail.com>
2022-11-06 13:39:07 +0300

    feat: find secret folder path

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,1 +1,6 @@
- 
+const { stdout } = process;
+const fs = require('fs/promises');
+const path = require('path');
+const pathToFolder = path.join(__dirname, 'secret-folder');
+
+stdout.write(pathToFolder);

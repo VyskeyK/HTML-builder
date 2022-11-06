c547f2e934d7af0e3fea859a6436dfd92ab3b019
VyskeyK <robintyan96@gmail.com>
2022-11-06 15:13:42 +0300

    feat: find files in styles folder

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,1 +1,21 @@
- 
+const { stdout } = require('process');
+const path = require('path');
+const { readdir } = require('fs/promises');
+
+const stylesDir = path.join(__dirname, 'styles');
+const bundleDir = path.join(__dirname, 'project-dist');
+
+bundleStyles();
+
+async function bundleStyles() {
+  try {
+    const files = await readdir(stylesDir, {withFileTypes: true});
+    const filteredFiles = files.filter( (file) => file.isFile());
+    for (const file of filteredFiles) {
+      console.log(file.name)
+    }
+
+  } catch (err) {
+    console.error(err.message);
+  }
+}

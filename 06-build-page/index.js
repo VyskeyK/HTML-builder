92261c40d3a0c402cf1779fe562a097700e67eb1
VyskeyK <robintyan96@gmail.com>
2022-11-06 15:36:55 +0300

    feat: read template.html

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,9 +1,10 @@
 const path = require('path');
-const { mkdir} = require('fs/promises');
+const { mkdir, readFile} = require('fs/promises');
 
 const distDir = path.join(__dirname, 'project-dist');
 
 createDist();
+bundleHTML();
 
 async function createDist() {
   try {
@@ -11,4 +12,10 @@ async function createDist() {
   } catch (err) {
     console.error(err.message);
   }
+}
+
+async function bundleHTML() {
+  const templatePath = path.join(__dirname, 'template.html');
+  const templateSource = await readFile(templatePath, 'utf-8');
+  console.log(templateSource);
 }

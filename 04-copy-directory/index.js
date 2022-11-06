54702a879498d7185ac6d2443431f1ab2f8c685b
VyskeyK <robintyan96@gmail.com>
2022-11-06 14:36:00 +0300

    feat: create empty copy folder

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,16 @@
- 
+const { stdout } = require('process');
+const path = require('path');
+const { mkdir } = require('fs/promises');
+
+const oldDir = path.join(__dirname, 'files');
+const newDir = path.join(__dirname, 'files-copy');
+
+copyDir();
+
+async function copyDir() {
+  try {
+    await mkdir(newDir, { recursive: true });
+  } catch (err) {
+    console.error(err.message);
+  }
+}

8689f81e1291a7aa4bee55a9aba098317b1676e8
VyskeyK <robintyan96@gmail.com>
2022-11-06 15:33:40 +0300

    feat: create project dist folder

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,1 +1,14 @@
- 
+const path = require('path');
+const { mkdir} = require('fs/promises');
+
+const distDir = path.join(__dirname, 'project-dist');
+
+createDist();
+
+async function createDist() {
+  try {
+    await mkdir(distDir, { recursive: true });
+  } catch (err) {
+    console.error(err.message);
+  }
+}

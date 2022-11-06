3b955cfe5b3f13e8ef6210ba3008a2fb7b4d2eca
VyskeyK <robintyan96@gmail.com>
2022-11-06 13:48:07 +0300

    feat: find files in secret dir

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,6 +1,17 @@
 const { stdout } = process;
-const fs = require('fs/promises');
+const {readdir} = require('fs/promises');
 const path = require('path');
 const pathToFolder = path.join(__dirname, 'secret-folder');
 
 stdout.write(pathToFolder);
+
+findFilesInDir(pathToFolder);
+
+async function findFilesInDir(path) {
+  try {
+    const files = await readdir(path);
+    for (const file of files) stdout.write(file);
+  } catch (err) {
+    console.error(err);
+  }
+}

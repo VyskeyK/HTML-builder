f3efcd2b24dba4eb7dfeb8d6462dd2f400bbadfd
VyskeyK <robintyan96@gmail.com>
2022-11-06 14:38:26 +0300

    feat:find files in folder for copy

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,6 +1,6 @@
 const { stdout } = require('process');
 const path = require('path');
-const { mkdir } = require('fs/promises');
+const { mkdir, readdir } = require('fs/promises');
 
 const oldDir = path.join(__dirname, 'files');
 const newDir = path.join(__dirname, 'files-copy');
@@ -10,6 +10,11 @@ copyDir();
 async function copyDir() {
   try {
     await mkdir(newDir, { recursive: true });
+    const files = await readdir(oldDir, {withFileTypes: true});
+    const filteredFiles = files.filter( (file) => file.isFile());
+    for (const file of filteredFiles) {
+      stdout.write(file.name + '\n');
+    }
   } catch (err) {
     console.error(err.message);
   }

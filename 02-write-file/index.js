1a522c4483e11cc41a174466ae0c0bac45d43694
VyskeyK <robintyan96@gmail.com>
2022-11-07 21:53:57 +0300

    fix: change created .txt path

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,7 +1,8 @@
 const { stdin, stdout } = process;
 const fs = require('fs');
+const path = require('path');
 
-const output = fs.createWriteStream('text.txt');
+const output = fs.createWriteStream(path.join(__dirname, 'text.txt'));
 
 stdout.write('Привет!\nНиже введи свое сообщение, и оно будет записано в файл text.txt\nЧтобы закончить, введи exit или Ctrl+C\n');
 
